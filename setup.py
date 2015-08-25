aacd0dba2d8fa6bb1cc5599a02a269b236125ddc
Jonatan Samoocha <jsamoocha@xebia.com>
2015-08-25 20:55:48 +0200

    Added initial setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pysweat',
+    version='0.1.dev1',
+    packages=[''],
+    url='https://github.com/jsamoocha/pysweat',
+    license='Apache',
+    author='Jonatan Samoocha',
+    author_email='',
+    description=''
+)
