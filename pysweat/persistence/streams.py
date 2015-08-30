26aaab7bb8c6c78e876bd96cead71f29e17896c6
Jonatan Samoocha <jsamoocha@xebia.com>
2015-08-30 16:10:25 +0200

    Simplified loading streams by using find_one - assumption is that activity_id and stream type form a unique key.

--- pysweat/persistence/streams.py
+++ pysweat/persistence/streams.py
@@ -1,2 +1,2 @@
 def load_stream(mongo, activity_id, stream_type):
-    return mongo.db.streams.find({u'activity_id': activity_id, u'type': stream_type})
+    return mongo.db.streams.find_one({u'activity_id': activity_id, u'type': stream_type})
--- tests/test_persist_streams.py
+++ tests/test_persist_streams.py
@@ -7,13 +7,12 @@ class StreamPersistenceTest(unittest.TestCase):
     @patch('pymongo.MongoClient')
     def test_load_stream_by_activity_id_and_type(self, mongo_mock):
         """Should load single stream"""
-        mongo_mock.db.streams.find.return_value = iter([{
+        mongo_mock.db.streams.find_one.return_value = {
             u'_id': u'123',
             u'activity_id': 456,
             u'data': [101, 102, 103],
             u'type': u'time'
-        }])
+        }
 
-        result = list(load_stream(mongo_mock, activity_id=456, stream_type=u'time'))
-        self.assertEqual(len(result), 1)
-        self.assertIn({u'_id': u'123', u'activity_id': 456, u'data': [101, 102, 103], u'type': u'time'}, result)
+        result = load_stream(mongo_mock, activity_id=456, stream_type=u'time')
+        self.assertDictEqual(result, {u'_id': u'123', u'activity_id': 456, u'data': [101, 102, 103], u'type': u'time'})
