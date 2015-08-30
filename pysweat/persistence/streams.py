8c481c06a6dc54470b7057b8c2da4b8ba9d03cc2
Jonatan Samoocha <jsamoocha@xebia.com>
2015-08-30 21:36:42 +0200

    Corrected loading of streams by returning a pandas dataframe instead of dict of raw data

--- pysweat/persistence/streams.py
+++ pysweat/persistence/streams.py
@@ -1,2 +1,7 @@
+import pandas as pd
+
+
 def load_stream(mongo, activity_id, stream_type):
-    return mongo.db.streams.find_one({u'activity_id': activity_id, u'type': stream_type})
+    index_stream = mongo.db.streams.find_one({'activity_id': activity_id, 'type': 'time'})
+    data_stream = mongo.db.streams.find_one({'activity_id': activity_id, 'type': stream_type})
+    return pd.DataFrame({stream_type: data_stream['data']}, index=index_stream['data'])
--- tests/test_persist_streams.py
+++ tests/test_persist_streams.py
@@ -1,18 +1,22 @@
 import unittest
 from mock import patch
+import pandas as pd
 from pysweat.persistence.streams import load_stream
 
 
 class StreamPersistenceTest(unittest.TestCase):
     @patch('pymongo.MongoClient')
     def test_load_stream_by_activity_id_and_type(self, mongo_mock):
-        """Should load single stream"""
+        """Should load single stream as 1-column dataframe, indexed by time"""
         mongo_mock.db.streams.find_one.return_value = {
-            u'_id': u'123',
-            u'activity_id': 456,
-            u'data': [101, 102, 103],
-            u'type': u'time'
+            '_id': '123',
+            'activity_id': 456,
+            'data': [101, 102, 103],
+            'type': 'velocity_smooth'
         }
 
-        result = load_stream(mongo_mock, activity_id=456, stream_type=u'time')
-        self.assertDictEqual(result, {u'_id': u'123', u'activity_id': 456, u'data': [101, 102, 103], u'type': u'time'})
+        result = load_stream(mongo_mock, activity_id=456, stream_type='velocity_smooth')
+        self.assertIs(type(result), pd.DataFrame)
+        self.assertEqual(list(result.columns.values), ['velocity_smooth'])
+        self.assertEqual(list(result.velocity_smooth.values), [101, 102, 103])
+        self.assertEqual(list(result.index.values), [101, 102, 103])
