3eadf5d168cbf44f506ffcd6ab433c6b6ee1d568
Jonatan Samoocha <jsamoocha@xebia.com>
2015-09-05 11:48:35 +0200

    Reordered assertion arguments for consistency with other tests

--- tests/test_transform_similarities.py
+++ tests/test_transform_similarities.py
@@ -8,25 +8,25 @@ class SimilarityTransformationTest(unittest.TestCase):
         v1 = (1, 1)
         v2 = (2, 2)
 
-        self.assertAlmostEqual(1, cosine_similarity(v1, v2), 9)
+        self.assertAlmostEqual(cosine_similarity(v1, v2), 1, 9)
 
     def test_cosine_similarity_orthogonal_vectors_2d(self):
         """Should return 0 for orthogonal vectors"""
         v1 = (1, 1)
         v2 = (2, -2)
 
-        self.assertAlmostEqual(0, cosine_similarity(v1, v2), 9)
+        self.assertAlmostEqual(cosine_similarity(v1, v2), 0, 9)
 
     def test_cosine_similarity_opposite_vectors_2d(self):
         """Should return -1 for vectors pointing in opposite directions"""
         v1 = (1, 1)
         v2 = (-2, -2)
 
-        self.assertAlmostEqual(-1, cosine_similarity(v1, v2), 9)
+        self.assertAlmostEqual(cosine_similarity(v1, v2), -1, 9)
 
     def test_cosine_similarity_similar_vectors_3d_lists(self):
         """Should compute cosine similarity regardless vector representation or dimension"""
         v1 = [1, 1, 1]
         v2 = [2, 2, 2]
 
-        self.assertAlmostEqual(1, cosine_similarity(v1, v2), 9)
+        self.assertAlmostEqual(cosine_similarity(v1, v2), 1, 9)
