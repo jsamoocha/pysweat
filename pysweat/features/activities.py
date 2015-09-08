Corrected turns_per_km feature computation, forgot to take smoothed x and y

pysweat/features/activities.py
@@ -15,10 +15,10 @@ class ActivityFeatures(object):
         stream_df = lat_long_to_x_y(stream_df)
         stream_df = smooth(stream_df, smooth_colname='x')
         stream_df = smooth(stream_df, smooth_colname='y')
-        stream_df = derivative(stream_df, derivative_colname='x')
-        stream_df = derivative(stream_df, derivative_colname='y')
-        stream_df = rolling_similarity(stream_df, cosine_similarity, 'dx_dt', 'dy_dt')
-        return np.nansum([cosine_to_deviation(cos) for cos in stream_df.cosine_similarity_dx_dt_dy_dt])
+        stream_df = derivative(stream_df, derivative_colname='x_smooth')
+        stream_df = derivative(stream_df, derivative_colname='y_smooth')
+        stream_df = rolling_similarity(stream_df, cosine_similarity, 'dx_smooth_dt', 'dy_smooth_dt')
+        return np.nansum([cosine_to_deviation(cos) for cos in stream_df.cosine_similarity_dx_smooth_dt_dy_smooth_dt])
 
     def turns_per_km(self, activity_df):
         activity_df['turns_per_km'] = [self.__total_deviation(activity_df.strava_id[i]) / activity_df.distance[i]