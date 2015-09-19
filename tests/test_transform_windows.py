c14314947cc493d10736e7858d2dd3d4a8ee979c
Jonatan Samoocha <jsamoocha@xebia.com>
2015-09-19 15:30:44 +0200

    Added specs to the tests

--- tests/test_transform_windows.py
+++ tests/test_transform_windows.py
@@ -3,13 +3,17 @@ from pysweat.transformation.windows import subtract_n_minutes
 
 class WindowsTransformTest(unittest.TestCase):
     def test_subtract_5_minutes(self):
+        """Should default to 5 minutes"""
         self.assertEqual(subtract_n_minutes(423), 123)
 
     def test_subtract_5_minutes_negative_result(self):
+        """Should not return negative values, but default to a minimum of 0"""
         self.assertEqual(subtract_n_minutes(99), 0)
 
     def test_subtract_5_minutes_minimum_value(self):
+        """Should return the provided minimum value in case of negative result"""
         self.assertEqual(subtract_n_minutes(99, minimum_value=50), 50)
 
     def test_subtract_1_minute(self):
+        """Should return result for a provided amount of minutes to be subtracted"""
         self.assertEqual(subtract_n_minutes(423, 1), 363)
