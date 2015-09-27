d1abf2fe1ba6cf3a57ba0c662ff0182e053643e0
Jonatan Samoocha <jsamoocha@xebia.com>
2015-09-27 18:21:11 +0200

    Added feature for finding max value maintained for at least n minutes

--- pysweat/features/activities.py
+++ pysweat/features/activities.py
@@ -4,6 +4,7 @@ from pysweat.persistence.streams import load_stream
 from pysweat.transformation.gps import lat_long_to_x_y
 from pysweat.transformation.streams import smooth, derivative, rolling_similarity
 from pysweat.transformation.similarities import cosine_similarity, cosine_to_deviation
+from pysweat.transformation.windows import subtract_n_minutes
 
 
 class ActivityFeatures(object):
@@ -23,4 +24,20 @@ class ActivityFeatures(object):
     def turns_per_km(self, activity_df):
         activity_df['turns_per_km'] = [self.__total_deviation(activity_df.strava_id[i]) / activity_df.distance[i]
                                        for i in activity_df.index]
+        return activity_df
+
+    def max_value_maintained_for_n_minutes(self, activity_df, measurement='heartrate', window_size=5):
+        all_max_values = [float('NaN')] * len(activity_df)
+
+        for i in range(len(activity_df)):
+            heartrate_df = load_stream(self.database_driver, activity_df.strava_id[i], '%s' % measurement)
+            if heartrate_df is not None:
+                min_index = heartrate_df.index.min()
+                all_max_values[i] = np.nanmax(
+                    [heartrate_df.heartrate.loc[subtract_n_minutes(second,
+                                                                   minutes=window_size,
+                                                                   minimum_value=min_index):second].min()
+                     for second in heartrate_df.index])
+
+        activity_df['max_%s_%d_minutes' % (measurement, window_size)] = all_max_values
         return activity_df
--- tests/test_features_activities.py
+++ tests/test_features_activities.py
@@ -18,3 +18,22 @@ class ActivityFeaturesTest(unittest.TestCase):
         self.assertItemsEqual(list(features_result.columns.values), ['strava_id', 'distance', 'turns_per_km'])
         self.assertAlmostEqual(features_result.turns_per_km[0], 0.12, 2)
         self.assertAlmostEqual(features_result.turns_per_km[1], 0.06, 2)
+
+    @patch('pysweat.features.activities.load_stream')
+    def test_max_value_maintained_for_n_minutes(self, load_stream_mock):
+        """Should return the maximum heartrate that was maintained for at least 5 minutes by default"""
+        load_stream_mock.return_value = pd.DataFrame(
+            {'heartrate': [100, 115, 120, 100, 110]},
+            index=[180, 360, 540, 720, 900]
+        )
+        activity_df = pd.DataFrame(
+            {'strava_id': [1, 2], 'distance': [1, 2]}
+        )
+        activity_features = ActivityFeatures(MongoClient())
+
+        features_result = activity_features.max_value_maintained_for_n_minutes(activity_df)
+
+        self.assertItemsEqual(list(features_result.columns.values),
+                              ['strava_id', 'distance', 'max_heartrate_5_minutes'])
+        self.assertEqual(len(features_result), 2)
+        self.assertEqual(features_result.max_heartrate_5_minutes[0], 115)
