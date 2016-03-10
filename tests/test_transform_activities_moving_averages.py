4bb5959bc1007be232d88876b59ae659dd87df8d
Jonatan Samoocha <jsamoocha@xebia.com>
2016-03-10 14:14:39 +0100

    Starting move of activity-based sliding window operations to this package - adding select_window tests.

---
+++ tests/test_transform_activities_moving_averages.py
@@ -0,0 +1,52 @@
+import unittest
+import pandas as pd
+import numpy as np
+
+from datetime import datetime, timedelta
+
+
+def select_window(activities, date, window_size):
+    return activities[(activities.start_date_local <= date) &
+                      (activities.start_date_local > date - timedelta(days=window_size))]
+
+
+class ActivityMovingAverageTransformationTest(unittest.TestCase):
+    test_activities = pd.DataFrame().from_dict({
+        'start_date_local': [np.datetime64(ts) for ts in pd.date_range(end='2015-05-01', periods=3).tolist()],
+        'test_var': [1, 2, 3],
+        'distance': [1, 1, 2],
+        'average_speed': [18, 22, 12]
+    })
+
+    mock_athletes = [
+        {'id': 123},
+        {'id': 456}
+    ]
+
+    def test_select_window_end_ts_and_window_size_within_data(self):
+        """Should return dataframe with complete window"""
+        selected_activities = select_window(self.test_activities, self.test_activities.start_date_local[2], 2)
+
+        self.assertEqual(2, len(selected_activities))
+        self.assertEqual(2, selected_activities.test_var.values[0])
+        self.assertEqual(3, selected_activities.test_var.values[1])
+
+    def test_select_window_end_ts_after_last_activity_window_size_within_data(self):
+        """Should return last activity"""
+        selected_activities = select_window(self.test_activities, pd.tslib.Timestamp('2015-05-02'), 2)
+
+        self.assertEqual(1, len(selected_activities))
+        self.assertEqual(3, selected_activities.test_var.values[0])
+
+    def test_select_window_end_ts_after_last_activity_window_size_outside_data(self):
+        """Should return empty"""
+        selected_activities = select_window(self.test_activities, pd.tslib.Timestamp('2015-05-05'), 2)
+
+        self.assertEqual(0, len(selected_activities))
+
+    def test_select_window_end_ts_before_last_activity_window_size_outside_data(self):
+        """Should return first activity"""
+        selected_activities = select_window(self.test_activities, pd.tslib.Timestamp('2015-04-29'), 2)
+
+        self.assertEqual(1, len(selected_activities))
+        self.assertEqual(1, selected_activities.test_var.values[0])
