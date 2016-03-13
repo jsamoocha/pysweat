5cf507a5f8b58aad3fba390c9a029659d6e682b6
Jonatan Samoocha <jsamoocha@xebia.com>
2016-03-13 15:25:42 +0100

    Added function for getting athlete ids

--- pysweat/persistence/athletes.py
+++ pysweat/persistence/athletes.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
+
 def load_athletes(mongo, **query):
     return pd.DataFrame(list(mongo.db.athletes.find(query)))
+
+
+def get_athlete_ids(mongo):
+    return mongo.db.athletes.find().distinct('id')
