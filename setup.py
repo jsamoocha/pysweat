0c4c8f8da764ee4df00eba1622bdeec9b6e51274
Jonatan Samoocha <jsamoocha@xebia.com>
2017-08-03 11:20:32 +0200

    Bumped package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pysweat',
-    version='0.1.dev1',
+    version='0.1.dev2',
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     install_requires=[
         'pymongo>=3',
