7abcb1998a8ffd3e61131e95e023185444969ade
Jonatan Samoocha <jsamoocha@xebia.com>
2017-09-20 15:31:10 +0200

    Minor cosmetics

--- pysweat/transformation/general.py
+++ pysweat/transformation/general.py
@@ -8,5 +8,5 @@ def delta_constant(observations_df, constant, constant_description, measurement=
 
 
 def get_observations_without_feature(observations_df, feature_name):
-    return pd.isnull(observations_df[feature_name]) if feature_name in observations_df \
-        else [True] * len(observations_df.index)
+    return (pd.isnull(observations_df[feature_name]) if feature_name in observations_df
+            else [True] * len(observations_df.index))
