2d381f50de74316015700736864371416a685bd9
Jonatan Samoocha <jsamoocha@xebia.com>
2018-12-02 15:37:19 +0100

    Corrects test names

--- tests/test_features_activities.py
+++ tests/test_features_activities.py
@@ -6,7 +6,7 @@ from pysweat.features.activities import ActivityFeatures
 
 
 class ActivityFeaturesTest(unittest.TestCase):
-    def test_turns_per_km(self):
+    def test_sum_of_turns(self):
         lat_long_stream_df = pd.DataFrame(
             {'latlng': [[52.1, 5.3], [52.2, 5.4], [58, 5.5], [52.4, 5.4], [52.5, 5.3]]},
             index=[1, 2, 3, 4, 5])
@@ -14,7 +14,7 @@ class ActivityFeaturesTest(unittest.TestCase):
         total_turns_result = ActivityFeatures.sum_of_turns(lat_long_stream_df)
         self.assertAlmostEqual(0.12, total_turns_result, places=2)
 
-    def test_turns_per_km_no_turns(self):
+    def test_sum_of_turns_no_turns(self):
         lat_long_stream_df = pd.DataFrame(
             {'latlng': [[52.1, 5.3], [52.2, 5.3], [52.3, 5.3], [52.4, 5.3], [52.5, 5.3]]},
             index=[1, 2, 3, 4, 5])
